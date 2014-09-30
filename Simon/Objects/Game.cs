748d2c6dd5ce040e2e1d9728572a9e4af4b0f498
alinddicu <alinddicu@yahoo.com>
2014-09-30 20:08:44 +0200

    buttons actually blink

--- Simon/Objects/Game.cs
+++ Simon/Objects/Game.cs
@@ -1,24 +1,38 @@
 ﻿namespace Simon.Objects
 {
+    using System.Linq;
     using Simon.Components;
     using System.Collections.Generic;
     using System.Threading;
+    using System.Timers;
 
     public class Game
     {
-        private readonly IEnumerable<SimonButton> _buttons;
+        private readonly List<SimonButton> _buttons;
+        private int _currentButtonIndex;
+
+        private readonly System.Timers.Timer _aTimer = new System.Timers.Timer(10000);
 
         public Game(params SimonButton[] buttons)
         {
-            _buttons = buttons;
+            _buttons = buttons.ToList();
+            _aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+
+            _aTimer.Interval = 500;
         }
 
-        public void Blink() 
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
         {
-            foreach (var button in _buttons)
+            _buttons[_currentButtonIndex++].Blink();
+            if (_currentButtonIndex == _buttons.Count())
             {
-                button.Blink();
+                _aTimer.Enabled = false;
             }
+        }
+
+        public void Blink()
+        {
+            _aTimer.Enabled = true;
         }
     }
 }
